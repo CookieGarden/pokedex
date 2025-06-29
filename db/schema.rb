8177e33ec828e210127c047ebb2caca2aa2acfbd
ryo watanabe <ryo.watanabe@theport.jp>
2025-06-30 06:08:41 +0900

    update ポケモンを取得する

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_29_075447) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_29_202111) do
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 end
