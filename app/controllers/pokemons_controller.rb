0338dfccc332e27bf0d9b1fe0cee53e4b3abd38a
ryo watanabe <ryo.watanabe@theport.jp>
2025-06-29 17:58:01 +0900

    update ポケモンを取得する

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,8 +1,30 @@
+require "faraday"
+
 class PokemonsController < ApplicationController
   def index
-    @pokemons = "aaa"
   end
 
   def show
+    id = params[:id]
+
+    # ポケモン種族情報を取得（名前用）
+    species_response = Faraday.get("https://pokeapi.co/api/v2/pokemon-species/#{id}/")
+    @response = JSON.parse(species_response.body)
+
+    # ポケモン基本情報を取得（画像用）
+    pokemon_response = Faraday.get("https://pokeapi.co/api/v2/pokemon/#{id}/")
+    @pokemon_data = JSON.parse(pokemon_response.body)
+
+    # 日本語名を取得
+    if @response && @response["names"]
+      japanese_name_entry = @response["names"].find { |name| name["language"]["name"] == "ja" }
+      @name = japanese_name_entry ? japanese_name_entry["name"] : @response["name"]
+    else
+      @name = "不明"
+    end
+
+    # 画像URLを取得
+    @image_url = @pokemon_data.dig("sprites", "other", "official-artwork", "front_default") || 
+                 @pokemon_data.dig("sprites", "front_default")
   end
 end
